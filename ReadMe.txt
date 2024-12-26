b14471beb89d0591528bc29ab6a32e3a67ad4924
aidan <aidantshea@gmail.com>
2024-12-25 17:32:12 -0800

    completed code to read in fasta files. Added MyPy and updated ReadMe

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,4 +1,4 @@
-conda create -n qPCR biopython spyder-kernels -y
+conda create -n qPCR biopython spyder-kernels mypy -y
 
 San1 gene pulled from
 https://www.ncbi.nlm.nih.gov/gene/851721
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from Bio import SeqIO
 
 # path to San1 gene of interest
-filepath = r"C:\_repos\qPCR Primer Algorithm\SAN1_datasets\ncbi_dataset\data\gene.fna"
+filepath = r"C:\_repos\qPCR-Primer-Designer\SAN1_datasets\ncbi_dataset\data\gene.fna"
 
 # this method reads in a fasta file and returns its records as a nested list
-def read_fasta(infile):
+def read_fasta(infile: str) -> list[list[str]]:
 
     # attempts to parse file from given path as .fasta, iteratively appending its records
     try:
-        records: list = []
+        print(f"attempting to pull records from the following path: {infile}")
+        records: list = []        
         for element in SeqIO.parse(infile, 'fasta'):
             ID: str = element.id; Seq: str = element.seq
-            records.append([ID, Seq])
-            print(f"record added: {[ID, Seq]}")
+            record: list = [ID, Seq]
+            records.append(record)
+            print(f"record added: {record}")        
         return records
-    
+
     # exception handling: prints error to terminal and returns None
     except Exception as error:
         print(f"unable to read file, see exception: {error}")
-        return None
-
-sequences = read_fasta(filepath)
+        return [[]]
